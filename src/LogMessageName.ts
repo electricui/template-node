a8a69e2c8e81ce99d59c3b3517e4d0585749fed7
Michael Orenstein <michael@whiteshadows.me>
2022-09-18 22:37:30 +0930

    wip: Node template basics

---
+++ src/LogMessageName.ts
@@ -0,0 +1,7 @@
+export enum LogMessageName {
+  UNNAMED = 0,
+  EXCEPTION = 1,
+  POLL = 2,
+  HINT_GENERATION = 3,
+  METADATA_INFO = 4,
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,43 @@
-import { Message } from '@electricui/core'
+import * as testCases from './tests/'
+
+import { Configuration, StreamReport } from '@electricui/script-utilities'
+import { LogMessageName } from './LogMessageName'
+
+const configuration = new Configuration({
+  logFilters: [],
+  enableColors: true,
+  enableHyperlinks: false,
+  enableProgressBars: true,
+  preferTruncatedLines: true,
+  enableTimers: true,
+  displayTimerThreshold: 2,
+})
 
 async function main() {
-  const message = new Message('msgId', 42)
+  console.log(Object.keys(testCases))
 
-  console.log(`the answer is ${message.messageID}`)
+  const forgettableNames = new Set([
+    LogMessageName.POLL,
+    LogMessageName.HINT_GENERATION,
+    LogMessageName.METADATA_INFO,
+  ])
+
+  const installReport = await StreamReport.start(
+    {
+      configuration,
+      json: false,
+      stdout: process.stdout,
+      includeLogs: true,
+      forgettableNames,
+    },
+    async report => {
+      for (const closure of Object.values(testCases)) {
+        await closure(report)
+      }
+    },
+  )
+
+  process.exit(installReport.exitCode())
 }
 
 main()
