01d997ff13af2bc444aac29b1b29a46198694ed8
Michael Orenstein <michael@whiteshadows.me>
2022-11-12 14:43:32 +1030

    refactor: Updated to Rollup v3

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-import run from '@rollup/plugin-run'
-import typescript from '@rollup/plugin-typescript'
-import autoExternal from 'rollup-plugin-node-externals'
-import commonjs from '@rollup/plugin-commonjs'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import path from 'path'
+const run = require('@rollup/plugin-run')
+const typescript = require('@rollup/plugin-typescript')
+const { externals } = require('rollup-plugin-node-externals')
+const commonjs = require('@rollup/plugin-commonjs')
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const path = require('path')
 
 const euiLoader = require('@electricui/rollup-loader')
 
@@ -13,13 +13,14 @@ export default {
     file: 'dist/index.js',
     format: 'cjs',
     sourcemap: true,
+    interop: "auto" // match typescript es module interop behaviour
   },
   plugins: [
     euiLoader(),
 
-    autoExternal({
+    externals({
       packagePath: path.resolve(__dirname, 'package.json'),
-      exclude: /electricui/,
+      //  exclude: /electricui/,
     }),
 
     nodeResolve(),
